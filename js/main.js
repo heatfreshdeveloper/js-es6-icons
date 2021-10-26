9723025df8c575da9916320fafecf6858e221ea4
heatfreshdeveloper <72042291+heatfreshdeveloper@users.noreply.github.com>
2021-10-26 17:41:40 +0200

    completato lo script per filtrare le icone

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-[
+const listaIcone = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -96,3 +96,48 @@
 		family: 'fas'
 	}
 ];
+
+const mappaColori = {
+    "animal": "blue",
+    "vegetable": "orange",
+    "user": "purple"
+}
+
+const containerIcons = document.querySelector(".card-container");
+const selectTypes = document.getElementById("filter_select")
+
+function stampCard (cards) {
+    containerIcons.innerHTML = "";
+for (let i = 0; i < cards.length; i++) {
+    const icona = cards[i];
+    const {family, prefix, name, type} = icona;
+    const colori = mappaColori[type];
+
+    
+    containerIcons.innerHTML += `
+    <div class="col">
+      <div class="card h-100 shadow-sm">
+        <div class="card-img-top">
+          <i class="${family} ${prefix + name}" style="color: ${colori}"></i>
+        </div>
+        <div class="card-body">
+          <div class="card-title text-center">${name}</div>
+        </div>
+      </div>
+    </div>`;
+
+}
+
+}
+
+stampCard(listaIcone);
+
+selectTypes.addEventListener("change", function() {
+    const types = listaIcone.filter((icon)=>{
+        if(icon.type === this.value || this.value === "") {
+            return true 
+        }
+    }) 
+
+    stampCard(types)
+})
